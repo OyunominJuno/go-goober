7d2fc38362ed49fb72abfcd77b412d3de9900e31
Oyunomin Munkhkhurel <junomunkhkhurel@Oyunomins-MacBook-Pro.local>
2026-04-22 15:10:52 -0700

    working api with mock up map data

--- api/api.go
+++ api/api.go
@@ -1,5 +1,10 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // Define params for the API request
 type CoinBalanceRequest struct {
 	Username string
@@ -14,3 +19,24 @@ type ErrorResponse struct {
 	Code    int
 	Message string
 }
+
+func returnError(h http.ResponseWriter, code int, message string) {
+	error := ErrorResponse{
+		Code:    code,
+		Message: message,
+	}
+
+	h.Header().Set("Content-Type", "application/json")
+	h.WriteHeader(code)
+
+	json.NewEncoder(h).Encode(error)
+}
+
+var (
+	UserCausedError = func(h http.ResponseWriter, err error) {
+		returnError(h, http.StatusBadRequest, err.Error())
+	}
+	ServerCausedError = func(h http.ResponseWriter) {
+		returnError(h, http.StatusInternalServerError, "Internal Server Error")
+	}
+)
--- internal/handlers.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-import (
-	"github.com/go-chi/chi"
-)
-
-func Handler(r *chi.Mux) {
-
-}
